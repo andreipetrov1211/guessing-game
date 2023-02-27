399021afa608c2a891da26376c319f667e85c760
Petrov Andrey <andreiptrz@yandex.ru>
2023-02-27 22:19:19 +0200

    fix: fixed function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min = 0;
+      this.max = 0;
+      this.guessNum = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
-
+  
     guess() {
-
+      this.guessNum = Math.round((this.min + this.max) / 2);
+      return this.guessNum;
     }
-
+  
     lower() {
-
+      this.max = this.guessNum;
     }
-
+  
     greater() {
-
+      this.min = this.guessNum;
     }
-}
+  }
 
 module.exports = GuessingGame;
